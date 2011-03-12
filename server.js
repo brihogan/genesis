c2af75e06ef8153bed06e9c1a879b6a547a5c262
Brian Hogan <brihogan@gmail.com>
2011-03-12 09:23:54 -0600

    hello user server

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+require("http").createServer(function (req, res) {
+  res.writeHead(200, {});
+  res.end("Hello, user!");
+}).listen(80);
