bb1c8f4ffd6f95f4633a3020f496ac46e6f8693c
Brian Hogan <brihogan@gmail.com>
2011-10-06 14:42:59 -0400

    Closes #2 and #3

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ require("http").createServer(function (req, res) {
 	res.writeHead(200, {});
 	// This is a simple app.
 	res.end("Hello, user! You are awesome. That is all.");
+	// Pretend I added the ability to move the selector up/down
+	// Pretend I fixed the bug #3
 }).listen(80);
