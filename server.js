8b0f4d88e17f0ae161555449a50ab98fb9ec353d
Brian Hogan <brihogan@gmail.com>
2011-10-06 14:49:10 -0400

    Closes #4. Closes #5.
    Can I link to issue #3? Or do I have to do gh-3?

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ require("http").createServer(function (req, res) {
 	res.end("Hello, user! You are awesome. That is all.");
 	// Pretend I added the ability to move the selector up/down
 	// Pretend I fixed the bug #3
+	// close test issue #5
+	// close test issue #4
+	// comment on #3
 }).listen(80);
